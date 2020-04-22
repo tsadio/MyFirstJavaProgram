11c6b60ccbe5c08a3c2f21a925bb935446a0a47f
tsadio <tsadio@montgomerycollege.edu>
2020-04-22 13:04:19 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("My name is Theo");
+    }
+}
